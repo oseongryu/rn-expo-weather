6d9ea78a27471ee9e18f9d312bdc252a924c9107
oseongryu <oseongryu@gmail.com>
2021-08-15 08:50:16 +0900

    [1]

--- App.js
+++ App.js
@@ -1,21 +1,57 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StyleSheet, Text, View } from 'react-native';
+import Loading from './Loading';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default function App(){
+  return <Loading />;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+
+
+
+// export default function App() {
+//   return (
+//     // <View style={styles.container}>
+//     //   <Text style={styles.text}>Hello</Text>
+//     //   <Text style={styles.text}>Hello</Text>
+//     //   <StatusBar style="auto" />
+//     // </View>
+//     <View style={styles.container}>
+//       <View style={styles.yellowView} >
+//         <Text style={styles.text}>Hello</Text>
+//       </View>
+//       <View style={styles.blueView} >
+//         <Text style={styles.text}>Hello</Text>
+//       </View>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     flexDirection: 'row',
+//     backgroundColor: '#fff',
+//     // alignItems: 'center',
+//     // justifyContent: 'center',
+//   },
+//   text: {
+//     color: 'green',
+//     fontSize: 20,
+//   },
+//   yellowView: {
+//     flex: 1,
+//     color: 'yellow',
+//     backgroundColor: 'yellow',
+//     fontSize: 20,
+//   },
+//   blueView: {
+//     flex: 2,
+//     color: 'blue',
+//     backgroundColor: 'blue',
+//     fontSize: 20,
+//   },
+// });
---
+++ README.md
@@ -0,0 +1,18 @@
+## 
+node -v
+npm -v
+npm install -g expo-cli
+expo init rn-expo-weather
+
+cd rn-expo-weather
+yarn start # you can open iOS, Android, or web from here, or run them directly with the commands below.
+yarn android
+yarn ios
+yarn web
+
+## reloading
+cmd + R
+cmd + D
+
+## Chrome Extensions
+ColorZilla
